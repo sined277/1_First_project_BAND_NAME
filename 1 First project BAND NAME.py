396a40a456cffeb44189ce5aa41b274810e29d39
Dennis <denchikdron228@gmail.com>
2023-05-12 14:31:17 +0300

    Initial commit

---
+++ 1 First project BAND NAME.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band name Generator.')
+name_one = input("What's the name of the city you grew up in? ")
+name_two = input("What's your pet's name? ")
+print('Your band name coild be', name_one, name_two)
